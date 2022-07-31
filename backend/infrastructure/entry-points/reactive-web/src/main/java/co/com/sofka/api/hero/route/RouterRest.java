60282bac1400ee7a1b772e43cd1cb400ae9f67d7
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-31 17:56:37 -0500

    Se dividen las rutas para cada handler

--- backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/hero/route/RouterRest.java
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/hero/route/RouterRest.java
@@ -1,6 +1,6 @@
-package co.com.sofka.api;
+package co.com.sofka.api.hero.route;
 
-import co.com.sofka.api.hero.handler.Handler;
+import co.com.sofka.api.hero.handler.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -12,14 +12,29 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 
 @Configuration
 public class RouterRest {
-@Bean
-public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-    return route(GET("/api/hero"), handler::getAllHeroUseCase)
-            .and(route(GET("/api/hero/{id}"), handler::getHeroUseCase))
-            .and(route(GET("/api/hero/{name}"), handler::getNameHeroUseCase))
-            .and(route(POST("/api/hero/create"), handler::createHeroUseCase))
-            .and(route(PUT("/api/hero/{id}"), handler::updateHeroUseCase))
-            .and(route(DELETE("/api/hero/{id}"), handler::deleteHeroUseCase));
+    @Bean
+    public RouterFunction<ServerResponse> routerCreateHeroFunction(CreateHeroHandler handler) {
+        return route(POST("/api/hero"), handler::createHero);
+    }
 
+    @Bean
+    public RouterFunction<ServerResponse> routerFindAllHeroesFunction(GetAllHeroesHandler handler) {
+        return route(GET("/api/hero"), handler::getAllHeroes);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerGetHeroFunction(GetHeroHandler handler) {
+        return route(GET("/api/hero/{id}"), handler::getHero);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerUpdateHeroFunction(UpdateHeroHandler handler) {
+        return route(PUT("/api/hero/{id}"), handler::updateHero);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerDeleteHeroFunction(DeleteHeroHandler handler) {
+        return route(DELETE("/api/hero/{id}"), handler::deleteHero);
     }
 }
+
